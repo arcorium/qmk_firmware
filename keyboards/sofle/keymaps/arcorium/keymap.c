2954d9803d23f447078e5c9a65e985d5f0cd6ab6
arcorium <arcorium.l@gmail.com>
2025-01-25 12:23:08 +0700

    fix right shift/gui

--- keyboards/sofle/keymaps/arcorium/keymap.c
+++ keyboards/sofle/keymaps/arcorium/keymap.c
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-    KC_LSFT_CAP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SM_SW, MT(MOD_RALT, KC_ESC),
+    KC_LSFT_CAP, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SM_SW, KC_RSFT_WIN,
     KC_LCTL_WIN, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MPLY, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RALT_ESC,
           XXXXXXX, XXXXXXX, KC_LALT_TILDE, TL_LOWR, KC_SPC,                 KC_ENT, TL_UPPR, KC_RALT_B, XXXXXXX, XXXXXXX
   ),
